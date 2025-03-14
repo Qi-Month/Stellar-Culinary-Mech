00f167738b70343839053047c41a0ec0ebeea90e
Qi-Month <dy757410@163.com>
2025-03-15 01:27:15 +0800

    删除无中生有

--- kubejs/client_scripts/jei/Info.js
+++ kubejs/client_scripts/jei/Info.js
@@ -1,11 +1,11 @@
 JEIEvents.information((event) => {
-	function addJEIInfo(item) {
-		event.addItem(item, Component.translate(`jei.info.${name}`.replace(":", ".")))
+	function addJEIInfo(name) {
+		event.addItem(name, Component.translate(`jei.info.${name}`.replace(":", ".")))
 	}
 
 	Ingredient.of("#exdeorum:pebbles")
 		.getItemIds()
 		.forEach((pebbles) => {
-			addJEIInfo(pebbles)
+			addJEIInfo("pebbles")
 		})
 })
